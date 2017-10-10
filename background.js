1e570617ee946a1889580c7b1088862ba8f739a5
cola1129 <cola1129@gmail.com>
2017-10-10 11:40:21 +0800

    change: bound size, always on top, no frame, display normal

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create(
-    'main.html',
-    {
-      bounds: { width: 1024, height: 768}
-    }
-  );
+  chrome.app.window.create('main.html', {
+      bounds: {
+        width: 1024,
+        height: 700
+      },
+      alwaysOnTop: true,
+      state: 'normal',
+      frame: 'none'
+  });
 });
