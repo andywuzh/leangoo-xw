bad3b1ffd650b59ffca7f42d1be533f834db28c6
cola1129 <cola1129@gmail.com>
2017-10-10 09:53:18 +0800

    webview fixed to the window width&height

--- main.js
+++ main.js
@@ -1,29 +1,8 @@
-var mainwin;
+function updateWebviews() {
+  var webview = document.getElementById("webview");
+  webview.style.height = document.documentElement.clientHeight + "px";
+  webview.style.width = document.documentElement.clientWidth + "px";
+};
 
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create(
-    'main.html',
-    {
-      id: "mainwin",
-      innerBounds: { width: 1024, height: 768}
-    },
-    function(win) {
-      mainwin = win;
-
-      win.contentWindow.addEventListener('DOMContentLoaded', onChange);
-      win.onBoundsChanged.addListener(onChange);
-
-      // var webview = win.contentWindow.document.getElementById('webview');
-      // webview.style.height = bounds.height + 'px';
-      // webview.style.width = bounds.width + 'px';
-    }
-  );
-});
-
-function onChange() {
-  var bounds = mainwin.innerBounds;
-
-  var webview = mainwin.contentWindow.document.getElementById('webview');
-  webview.style.height = bounds.height + 'px';
-  webview.style.width = bounds.width + 'px';
-}
+onload = updateWebviews;
+window.onresize = updateWebviews;
