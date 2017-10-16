ca6109065ba33b85c2e6f26c56f18f84b3646291
cola1129 <cola1129@gmail.com>
2017-10-16 19:45:21 +0800

    change: extend init width

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('main.html', {
       bounds: {
-        width: 1024,
+        width: 1400,
         height: 700
       },
       alwaysOnTop: true,
